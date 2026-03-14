57dbd8c2eb25e16baa925ffd4e51face91178627
baize520mc <122137196+baize520mc@users.noreply.github.com>
2026-03-14 13:19:33 +0800

    还原

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -79,33 +79,35 @@ class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
         );
 
     setupServerWidget() => Flexible(
-      child: Offstage(
-        offstage: true,   // 始终隐藏，避免显示广告提示
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(', ', style: TextStyle(fontSize: em)),
-            Flexible(
-              child: InkWell(
-                onTap: onUsePublicServerGuide,
-                child: Row(
-                  children: [
-                    Flexible(
-                      child: Text(
-                        translate('setup_server_tip'),
-                        style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            fontSize: em),
-                      ),
+          child: Offstage(
+            offstage: !(!_svcStopped.value &&
+                stateGlobal.svcStatus.value == SvcStatus.ready &&
+                _svcIsUsingPublicServer.value),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(', ', style: TextStyle(fontSize: em)),
+                Flexible(
+                  child: InkWell(
+                    onTap: onUsePublicServerGuide,
+                    child: Row(
+                      children: [
+                        Flexible(
+                          child: Text(
+                            translate('setup_server_tip'),
+                            style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                fontSize: em),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
     
     basicWidget() => Row(
           crossAxisAlignment: CrossAxisAlignment.center,
